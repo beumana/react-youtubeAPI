59132d28f9e30dae6e744263c300194f19fc432c
beumana <bryantumana@gmail.com>
2018-02-16 15:52:22 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCRAA1mlQoGjGMYrWBcBcBc14wVY2hM_pE';
+
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
