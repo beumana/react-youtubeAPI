b7f4633c9bee1cb171e465f1f0ae99ac59ff17ab
beumana <bryantumana@gmail.com>
2018-02-23 11:57:08 -0500

    change input to class component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCRAA1mlQoGjGMYrWBcBcBc14wVY2hM_pE';
 
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
